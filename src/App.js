7c48d878729bfef1a9a2ef29f38ee66cf461206a
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-07-28 22:18:04 -0500

    Connected to API, stored in local state, added search bar & button, created and rendered recipe component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipes from './Recipes';
 
 function App() {
+  const APP_ID = "8d5395c8";
+  const APP_KEY = "6962b2171332ed110a81f82a2b940e77";
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <Recipes />
     </div>
   );
-}
+};
 
 export default App;
