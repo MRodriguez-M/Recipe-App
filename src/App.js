bf7a20d6a0fd25e0dbf4d74325b0de7584340fca
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-07-29 21:35:56 -0500

    Control search bar input, set button to make query

--- src/App.js
+++ src/App.js
@@ -7,21 +7,27 @@ function App() {
   const APP_KEY = "6962b2171332ed110a81f82a2b940e77";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    getRecipes();
+    getRecipes(search);
   }, []);
 
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const getRecipes = async (query) => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
   };
 
+  const getSearch = (e) => {
+    e.preventDefault();
+    getRecipes(search);
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input value={search} onChange={(e) => setSearch(e.target.value)} className="search-bar" type="text"/>
         <button className="search-button" type="submit">
           Search
         </button>
