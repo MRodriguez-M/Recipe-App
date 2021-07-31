37dd2babafe6c2191270b678777f4028349fd328
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-07-31 02:28:46 -0500

    Implemented style component to create styled div for Card

---
+++ src/components/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 60%;
+  border: 2px solid gray;
+  border-radius: 10px;
+  padding: 25px;
+  margin: 10px;
+`;
