10a623a9f4556980639e77c463e9ad4b1375744a
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-07-31 23:08:27 -0500

    Parsed mealType to correct string, formatted calories and healthLabels

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,28 +2,36 @@ import React from 'react';
 import { Card } from './styles';
 
 const Recipe = (props) => {
-    const getMealType = () => {
-        const mealTypeStr = props.mealType.join(", ");
-        return mealTypeStr;
-    }
-    return (
-        <Card>
-            <h2>{props.name}</h2>
-            {props.mealType && <p>{getMealType()}</p>}
-            <p>{props.calories}</p>
-            <p>{props.healthLabels}</p>
-            <img src={props.image} alt={`${props.name}`}/>
-            <p>{props.dishType}</p>
-            <ul>
-                {
-                  props.ingredients.map(ingredient =>
-                    (
-                      <li>{ingredient}</li>
-                    ))
-                }
-            </ul>
-        </Card>
-    )
+  const getMealType = () => {
+    const resultArr = [];
+    for (let i = 0; i < props.mealType.length; i++) {
+      const tempArr = props.mealType[i].split("/");
+      for (let j = 0; j < tempArr.length; j++) {
+        tempArr[j] = tempArr[j].charAt(0).toUpperCase() + tempArr[j].slice(1);
+      }
+      resultArr.push(tempArr.join(", "));
+    }  
+    return resultArr.join(", ");
+  };
+
+  return (
+    <Card>
+      <h2>{props.name}</h2>
+      {props.mealType && <p>Meal Type: {getMealType()}</p>}
+      <p>{props.calories.toFixed(2)} kcals</p>
+      <p>{props.healthLabels[0]} | {props.healthLabels[1]} | {props.healthLabels[2]}</p>
+      <img src={props.image} alt={`${props.name}`}/>
+      <p>{props.dishType}</p>
+      <ul>
+        {
+          props.ingredients.map(ingredient =>
+            (
+              <li>{ingredient}</li>
+            ))
+        }
+      </ul>
+    </Card>
+  )
 }
 
 export default Recipe;
