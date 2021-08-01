f149bd758c32ed09f105bc7638e84ed3daedcd45
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2021-07-31 21:04:59 -0500

    Added props to Card, created function to format meal type prop

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Card } from './styles';
 
 const Recipe = (props) => {
+    const getMealType = () => {
+        const mealTypeStr = props.mealType.join(", ");
+        return mealTypeStr;
+    }
     return (
         <Card>
             <h2>{props.name}</h2>
-            <p>{props.dishType}</p>
+            {props.mealType && <p>{getMealType()}</p>}
+            <p>{props.calories}</p>
+            <p>{props.healthLabels}</p>
             <img src={props.image} alt={`${props.name}`}/>
+            <p>{props.dishType}</p>
             <ul>
                 {
                   props.ingredients.map(ingredient =>
